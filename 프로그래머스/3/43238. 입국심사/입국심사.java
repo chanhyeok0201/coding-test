87c10f6e05269b8da61cc3072a16f596d5175e79
chanhyeok0201 <91006942+chanhyeok0201@users.noreply.github.com>
2026-04-23 01:59:04 +0900

    [level 3] Title: 입국심사, Time: 42.47 ms, Memory: 94.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,37 @@
+class Solution {
+    public long solution(int n, int[] times) {
+        long left = 1;   
+        long right = 0;  
+        
+        for (int time : times) {
+            right = Math.max(right, time);
+        }
+        right *= n;
+        
+        long answer = right;
+        
+        while (left <= right) {
+            long mid = (left + right) / 2; 
+            
+            long count = 0;
+            
+            for (int time : times) {
+                count += mid / time;
+                
+                if (count >= n) {
+                    break;
+                }
+            }
+            
+            if (count >= n) {
+                answer = mid;
+                right = mid - 1;
+            } 
+            else {
+                left = mid + 1;
+            }
+        }
+        
+        return answer;
+    }
+}
